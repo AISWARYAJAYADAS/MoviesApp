6efa3375d070801409c65cdac93ff34aafe9a0ef
Aiswarya Jayadas <aiswarya.jayadas@qburst.com>
2023-11-23 13:49:24 +0530

    Added logging interceptor

--- app/src/main/java/com/example/movieapp/util/LoggingInterceptorImpl.kt
+++ app/src/main/java/com/example/movieapp/util/LoggingInterceptorImpl.kt
@@ -2,11 +2,15 @@ package com.example.movieapp.util
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 
 class LoggingInterceptorImpl : Interceptor {
+
+    private val loggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        return chain.proceed(request)
+        return loggingInterceptor.intercept(chain)
     }
 
 
